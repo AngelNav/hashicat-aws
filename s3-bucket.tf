b23a2282c5270d6eae0ba2eb019fd8128fc29af3
Ángel Hernández <52288023+AngelNav@users.noreply.github.com>
2023-04-04 11:39:00 -0600

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket" {
-  source  = "app.terraform.io/PalmaInc/s3-bucket/aws"
+  source  = "app.terraform.io/angel-nav/s3-bucket/aws"
   version = "2.8.0"
   bucket_prefix = var.prefix
 }
